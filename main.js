af3f271d8174735d10e8428d9799a516f9d5bf5c
Art <hello@artsegalas.com>
2020-06-08 01:18:13 +0200

    Add blog posts fetch

--- main.js
+++ main.js
@@ -2,6 +2,31 @@ const body = document.getElementsByTagName('body')[0]
 const header = document.getElementById('header')
 const navToggle = document.getElementById('nav-toggle')
 const questions = document.getElementsByClassName('faq-question')
+const news = document.getElementsByClassName('news-item')
+const newsUrl = 'https://5edd6a78ae06ed0016ee3557.mockapi.io/news'
+const months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+
+fetch(newsUrl)
+  .then(response => response.json())
+  .then(resNews => {
+		for (let i = 0; i < news.length; i++) {
+			let newsItem = news[i]
+			let newsItemImage = newsItem.getElementsByTagName('img')[0]
+			let newsItemDate = newsItem.getElementsByTagName('time')[0]
+			let newsItemTitle = newsItem.getElementsByTagName('h4')[0]
+			let newsItemDescription = newsItem.getElementsByTagName('p')[0]
+
+			let newsItemShortDate = resNews[i].createdAt.split('T')[0]
+			let newsItemDay = newsItemShortDate.split('-')[2]
+			let newsItemMonth = newsItemShortDate.split('-')[1]
+			let newsItemYear = newsItemShortDate.split('-')[0]
+
+			newsItemImage.src = resNews[i].image
+			newsItemDate.innerHTML = `${newsItemDay} ${months[+newsItemMonth]} ${newsItemYear}`
+			newsItemTitle.innerHTML = resNews[i].title
+			newsItemDescription.innerHTML = resNews[i].description
+  	}
+	});
 
 navToggle.addEventListener('click', function () {
   if (!header.classList.contains('open')) {
