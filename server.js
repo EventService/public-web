c7de7288bd4da2045f0abfe6eec5838ce13989c8
Tonya Lebedeva <tonya@byallmeans.work>
2020-07-15 17:58:44 +0200

    feat: rewrite to express + new images

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+app.get('/faq', (req, res) => res.sendFile(path.join(__dirname, 'faq.html')))
+app.get('/about-us', (req, res) => res.sendFile(path.join(__dirname, 'about-us.html')))
+app.get('/blogs', (req, res) => res.sendFile(path.join(__dirname, 'blogs.html')))
+app.get('/blogs/:blogId', (req, res) => {    
+    console.log('req.params.blogId ', req.params.blogId)
+    res.sendFile(path.join(__dirname, 'blog.html'))
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
